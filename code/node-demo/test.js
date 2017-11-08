747e7c5a58b39cbe9d0a19a539388897e52b6e3e
lihuan <li.huan@zhtu.net>
2017-11-08 10:26:03 +0800

    intro.js测试

--- code/node-demo/test.js
+++ code/node-demo/test.js
@@ -2,6 +2,24 @@
 // import introJs from "intro.js/intro.js"
 console.log('hello');
 introJs().start();
+introJs().showHints();
+
+// introJs().setOptions(
+//     {
+//       hints:[
+//       {
+//         element: '#one',
+//         hintPosition: 'bottom',
+//         hint: 'hello one!'
+//       },
+//       {
+//         element: '#two',
+//         hintPosition: 'right',
+//         hint: 'hello two!'
+//       }
+//       ]
+//     }
+//     ).addHints().start();
 function showToastr() {
     console.log('showToastr 11111');
     toastr.info('This is a info toastr');
