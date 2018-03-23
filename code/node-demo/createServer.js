7023b5176c0aaf661533b5a2e4aad8ae16f16194
lihuan <li.huan@zhtu.net>
2018-03-23 16:18:27 +0800

    删除无用文件，整理结构

--- code/node-demo/createServer.js
+++
@@ -1,5 +0,0 @@
-let http = require('http');
-http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text-plain'});
-    response.end('hello world\n');
-}).listen(8124);
