05ca40c3a93eae1b139373f101c74f2a3ac0cbc4
lihuan <li.huan@zhtu.net>
2018-05-23 18:11:41 +0800

    lite server bs-config

---
+++ bs-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    port: 8080,
+    ui: {
+      port: 8081,
+    },
+    files: [ // 监控文件范围
+      'index.html', // 根目录下entry collection
+      'www/**/*.{html,css,js}', // app输出
+    //   'wechat/**/*.{html,css,js}', // wechat输出
+    //   'page/**/*.{html,css,js}' // page输出
+    ],
+    // open: 'ui', // 启动后打开ui
+    browser: 'google chrome', // 使用chrome打开
+    reloadDebounce: 500, // 500ms后再刷新
+    https: true,
+  };
