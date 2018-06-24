5333d8657d2eeca1f84cca830357057d0fe071a3
li huan <li.huan@zhtu.net>
2018-06-24 20:22:35 +0800

    补充提交

--- project/ocx/ocx.ts
+++ project/ocx/ocx.ts
@@ -22,15 +22,6 @@ import JsSHA from 'jssha';
 })();
 
 async function getData(req: express.Request, res: express.Response) {
-    // xNBTeVygbSlF6g6TAr8tSXbTdfh91fthNmAlMriB
-    // 备注名 coinx-web
-    // 4a7cLSrZqdgrVxbL37w1FyxAfxX2wK2bx01tY8Qy
-    // 创建时间 2018-06-13 05:39:19
-    let shaObj = new JsSHA('SHA-1', 'TEXT');
-    // shaObj.update('jsapi_ticket=' + ticket + '&noncestr=' + nonceStr + '&timestamp=' + timeStamp + '&url=' + request.url);
-    let signature = shaObj.getHash('HEX'); // 对str使用sha1签名，得到signature
-    // let hash = HMAC-SHA256(payload, secret_key).to_hex 
-
     let response = await axios.request({
         url: `https://openapi.ocx.com/api/v2/tickers`,
         method: 'get',
