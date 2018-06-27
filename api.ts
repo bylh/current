65e65de2d27c261fa8d674336e8a0aed2c93e9c6
li huan <li.huan@zhtu.net>
2018-06-28 01:15:35 +0800

    post方法参数发送存在问题

--- api.ts
+++ api.ts
@@ -1,19 +1,33 @@
-import { getSignal } from './common';
+import { getSignal, postSignal } from './common';
 
 import express from 'express';
 
 export async function autoTrade(req: express.Request, res: express.Response) {
- 
-    console.log(req.query);
-    res.sendStatus(400);
+    console.log(req.body, req.query, req.params, req);
+    res.sendStatus(200);
     return;
-    if(req.body.key == null || req.body.sec == null){
+
+    if(req.query.key == null || req.query.sec == null || req.query.order == null){
         res.sendStatus(400);
         return;
     } 
-    let interval = setInterval(async () => {
-        let order = await getSignal(req.body.key, req.body.sec, 'orders', null);
+
+    try {
+        console.log('111111111111:', req.query.order, typeof(req.query.order));
+        await postSignal(req.query.key, req.query.sec, 'orders', req.query.order)
+        console.log('下单成功');
+    } catch(err) {
+        console.log('下单失败', err);
+    }
+
+    let order:any = null;
+    let interval = await setInterval(async () => {
+        order = await getSignal(req.query.key, req.query.sec, 'orders', null);
         console.log('获取订单成功');
-        res.status(200).json(order)
     }, 10000);
+    setTimeout(() => {
+        console.log('autoTrade:', order)
+        res.status(200).json(order)
+    }, 12000)
+   
 }
