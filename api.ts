60b5e53b7a1fbb1de42558fd0a0a4d6153d37f4f
li huan <li.huan@zhtu.net>
2018-07-03 01:31:35 +0800

    post data？

--- api.ts
+++ api.ts
@@ -3,18 +3,20 @@ import { getSignal, postSignal } from './common';
 import express from 'express';
 
 export async function autoTrade(req: express.Request, res: express.Response) {
-    console.log(req.body, req.query, req.params, req);
-    res.sendStatus(200);
-    return;
-
-    if(req.query.key == null || req.query.sec == null || req.query.order == null){
+    console.log(req.body, req.query, req.params,req);
+    
+    if(req.query.key == null || req.query.sec == null){
         res.sendStatus(400);
         return;
     } 
 
     try {
-        console.log('111111111111:', req.query.order, typeof(req.query.order));
-        await postSignal(req.query.key, req.query.sec, 'orders', req.query.order)
+        await postSignal(req.query.key, req.query.sec, 'orders', {
+            market_code: req.query.market_code,
+            side: req.query.side,
+            price: req.query.price,
+            volume: req.query.volume,
+        })
         console.log('下单成功');
     } catch(err) {
         console.log('下单失败', err);
