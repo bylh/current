1c946990e19c118c7c4e14fd7541cd9544474710
li huan <li.huan@zhtu.net>
2018-07-20 02:04:38 +0800

    pwa push

--- project/web/src/app/app.component.ts
+++ project/web/src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { ServiceWorkerModule } from '@angular/service-worker';
-import {BehaviorSubject} from 'rxjs';
+import urlb64touint8array from 'urlb64touint8array';
+// import webpush from 'web-push';
+import { BehaviorSubject } from 'rxjs';
 import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { identifierModuleUrl } from '../../node_modules/@angular/compiler';
 // import * as Pwa from '@angular/pwa';
+const publicKey = 'BJ3kbCc44PMG9THjY4Nc-JqYKsUkd64e-n4oFGErmuAuFfunVUK1hqrqLOHEO_L1KJQhAZgZSn4F8lUZCYhPRfk';
+const privateKey = 'XqOsx4D0H85wlCrQn3IMqZECs9mrGdwk8lMOrAvUFL0';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,24 +17,63 @@ export class AppComponent implements AfterViewInit, OnInit {
   protected sw: ServiceWorkerRegistration = null;
   protected pushSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   public async ngAfterViewInit() {
-    // console.log('hello');
-    // const interval = setInterval(async () => {
-    //   if (this.sw == null) {
-    //     this.sw = await navigator.serviceWorker.getRegistration('/ngsw-worker.js');
-    //     console.log('this.sw123: ', this.sw);
-    //   } else {
-    //     clearInterval(interval);
-    //   }
-    // }, 2000);
+    self.addEventListener('push', (event: any) => {
+      const notificationData = event.data.json();
+      const title = notificationData.title;
+      // 可以发个消息通知页面
+      // util.postMessage(notificationData);
+      // 弹消息框
+      console.log('收到消息');
+      event.waitUntil((self as any).registration.showNotification(title, notificationData));
+
+    });
   }
-  public ngOnInit() {
+  public async ngOnInit() {
     console.log('ngOninit');
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      navigator.serviceWorker.register('/ngsw-worker.js').then(reg => {
-        this.sw = reg;
-        // this.sw.pushManager.getSubscription()
-        console.log('Service Worker and Push is supported', this.sw);
-      });
+    if ('serviceWorker' in navigator) {
+      this.sw = await navigator.serviceWorker.getRegistration('/ngsw-worker.js');
+      if (this.sw == null) {
+        this.sw = await navigator.serviceWorker.register('/ngsw-worker.js');
+
+      }
     }
+    if ('PushManager' in window) {
+      const subscription = await this.sw.pushManager.getSubscription();
+      console.log('用户订阅的数据：', JSON.stringify(subscription));
+      if (subscription == null) {
+        console.log('订阅开始');
+        await this.subscribeUser(this.sw);
+        console.log('订阅成功');
+      } else {
+        console.log('you have subscribed');
+      }
+    }
+  }
+
+  public async subscribeUser(swRegistration) {
+    // const vapidKeys = webpush.generateVAPIDKeys();
+    // console.log(vapidKeys.publicKey, vapidKeys.privateKey);
+    const subscription = await swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlb64touint8array(publicKey)
+    });
+    if (subscription == null) {
+      return;
+    }
+    this.updateSubscriptionOnServer(subscription);
+  }
+  public updateSubscriptionOnServer(subscription) {
+    const url = 'https://node-web-push-app.azurewebsites.net/subscribe';
+
+    if (subscription) {
+      const subscriptionJson = JSON.stringify(subscription);
+      const fetchOptions = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: subscriptionJson };
+      fetch(url, fetchOptions)
+        .then(data => console.log('Push subscription request succeeded with JSON response', data))
+        .catch(error => console.log('Push subscription request failed', error));
+    }
+  }
+  public async push() {
+    console.log('测试推送');
   }
 }
