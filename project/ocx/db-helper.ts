08c250ffcd512163282256d537fc37cd6f62b08a
li huan <li.huan@zhtu.net>
2018-07-26 01:46:55 +0800

    null

--- project/ocx/db-helper.ts
+++ project/ocx/db-helper.ts
@@ -1,14 +1,17 @@
 
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 class DBHelper {
-    protected db: any = null;
+    protected db: Mongoose = null;
     public async init() {
         try {
-            await mongoose.connect('mongodb://bylh:439882@127.0.0.1:27018/web');
+            this.db = await mongoose.connect('mongodb://bylh:439882@127.0.0.1:27018/web');
         } catch(err) {
             throw err;
         }
     
+    }
+    public async get() {
+        
     }
     
 }
