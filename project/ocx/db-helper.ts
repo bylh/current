de513e7368754b0ad8531c2240735ecaab586102
li huan <li.huan@zhtu.net>
2018-07-26 01:17:49 +0800

    mongo web

--- project/ocx/db-helper.ts
+++ project/ocx/db-helper.ts
@@ -4,7 +4,7 @@ class DBHelper {
     protected db: any = null;
     public async init() {
         try {
-            await mongoose.connect('mongodb://admin:439882@127.0.0.1:27018/admin');
+            await mongoose.connect('mongodb://bylh:439882@127.0.0.1:27018/web');
         } catch(err) {
             throw err;
         }
