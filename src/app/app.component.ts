78f567b9d42b3b0347ec1e205c5f93549cd0aa29
lihuan <li.huan@zhtu.net>
2018-08-01 13:54:16 +0800

    添加登录注册重置密码

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
 import { ServiceWorkerModule, SwPush } from '@angular/service-worker';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
 import urlb64touint8array from 'urlb64touint8array';
 import axios from 'axios';
 // import webpush from 'web-push';
@@ -18,17 +19,31 @@ export class AppComponent implements AfterViewInit, OnInit {
   title = '我的空间';
   protected sw: ServiceWorkerRegistration = null;
   protected pushSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-  constructor(protected swPush: SwPush, protected appService: AppService, public afAuth: AngularFireAuth) {
+  constructor(
+    protected swPush: SwPush,
+    protected appService: AppService,
+    public afAuth: AngularFireAuth) {
   }
 
   public async ngAfterViewInit() {
-      // 监听推送消息
-      this.swPush.messages.subscribe(msg => {
-        console.log('收到推送消息', msg);
-      });
-
-
-
+    // 监听推送消息
+    this.swPush.messages.subscribe(msg => {
+      console.log('收到推送消息', msg);
+    });
+    // 监听用户状态信息
+    this.afAuth.authState.subscribe((user) => {
+      console.log('用户状态： ', user);
+      console.log('用户邮箱地址是否验证', user.emailVerified);
+    });
+    this.afAuth.user.subscribe(((user) => {
+      console.log('user:', user)
+    }));
+    this.afAuth.idToken.subscribe((idToken) => {
+      console.log('idToken:', idToken);
+    });
+    this.afAuth.idTokenResult.subscribe((idTokenResult) => {
+      console.log('idTokenResult:', idTokenResult);
+    });
   }
 
   public async ngOnInit() {
@@ -82,9 +97,18 @@ export class AppComponent implements AfterViewInit, OnInit {
     let result = await this.afAuth.auth.signInWithEmailAndPassword('494397353@qq.com', '439882');
   }
   public async signUp() {
-    let result = await this.afAuth.auth.createUserWithEmailAndPassword('494397353@qq.com', '439882');
+    // let result = await this.afAuth.auth.createUserWithEmailAndPassword('494397353@qq.com', '439882');
+    this.afAuth.auth.createUserAndRetrieveDataWithEmailAndPassword('494397353@qq.com', '439882')
+      .then(res =>  {
+        console.log('res:', res);
+        this.afAuth.auth.currentUser.sendEmailVerification();
+    })
+      .catch(err => console.log('err:', err));
   }
   public async logout() {
     let result = await this.afAuth.auth.signOut();
+  }
+  public async resetPwd() {
+    let result = await this.afAuth.auth.sendPasswordResetEmail('494397353@qq.com')
   }
 }
