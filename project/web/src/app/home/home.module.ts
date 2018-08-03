1cef9605ac11bb5adfa43f1fd010b0c1400a4c66
lihuan <li.huan@zhtu.net>
2018-08-03 17:47:16 +0800

    add material module

--- project/web/src/app/home/home.module.ts
+++ project/web/src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { AppMaterialModule } from '../app-material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    AppMaterialModule
   ],
   declarations: [HomeComponent]
 })
