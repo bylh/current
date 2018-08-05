dbc76e69a7a2fd07154b2033df4aecbcc873a2f5
li huan <li.huan@zhtu.net>
2018-08-05 14:38:59 +0800

    修改app service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,23 +1,25 @@
+import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
 import { interval } from 'rxjs';
 import { SwUpdate, SwPush } from '@angular/service-worker';
+import axios from 'axios';
+
+const publicKey = 'BJ3kbCc44PMG9THjY4Nc-JqYKsUkd64e-n4oFGErmuAuFfunVUK1hqrqLOHEO_L1KJQhAZgZSn4F8lUZCYhPRfk';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor(updates: SwUpdate, protected swPush: SwPush) {
+  constructor(protected updates: SwUpdate, protected swPush: SwPush) {
     // 监听推送消息
     swPush.messages.subscribe(msg => {
       console.log('收到推送消息', msg);
     });
 
-    // 30秒检查一次更新
-    interval(1000 * 30).subscribe(() => {
-      updates.checkForUpdate().then(() => console.log('检查更新'))
-        .catch(err => console.log('检查更新出现错误', err));
-    });
+    // 使用interval(1000 * 30).subscribe更新，不动
+    updates.checkForUpdate().then(() => console.log('检查更新'))
+      .catch(err => console.log('检查更新出现错误', err));
 
     // 是否需要更新
     updates.available.subscribe(event => {
@@ -31,6 +33,29 @@ export class AppService {
       console.log('new version is', event.current);
     });
   }
+
+  public async subscribeUser(): Promise<string> {
+    try {
+      const pushSubscription = await this.swPush.requestSubscription({
+        serverPublicKey: publicKey
+      });
+      console.log('subscribeUser(): 订阅成功');
+      // 订阅成功将信息保存到服务器
+      await axios.request({
+        url: `${environment.BaseUrl}/subscribe`,
+        method: 'post',
+        params: {
+          publicKey: publicKey,
+          pushSubscription: JSON.stringify(pushSubscription)
+        },
+      });
+      console.log('subscribeUser(): 订阅信息保存到服务器成功');
+      return JSON.stringify(pushSubscription);
+    } catch (err) {
+      throw err;
+    }
+  }
+
   getSwPushMsgOb() {
     return this.swPush.messages;
   }
