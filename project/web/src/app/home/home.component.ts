5ed5b0e75094c57f106021f044c1260a4c94501d
li huan <li.huan@zhtu.net>
2018-08-07 23:26:34 +0800

    解决angualr 跨域访问问题

--- project/web/src/app/home/home.component.ts
+++ project/web/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,17 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  async getPairs() {
+    try {
+      const res = await axios.request({
+        url: 'http://localhost:4200/data/api2/1/pairs',
+        method: 'get',
+      });
+      console.log('res:', res);
+    } catch (err) {
+      console.log('err:', err);
+    }
   }
 
 }
