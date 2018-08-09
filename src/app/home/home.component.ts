ad76940433ebc50e6c6e67de68958a9d7ea1c9da
li huan <li.huan@zhtu.net>
2018-08-10 02:11:27 +0800

    完善

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,10 @@ import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 export interface Coin {
-  symbol: string;
-  pair: string;
-  rate: string;
-  rate_percent: string;
+  symbol: string; // 代币简称 如ETH
+  pair: string; // 交易对
+  rate: string; // 兑换率
+  rate_percent: string; // 上涨下降比
 }
 @Component({
   selector: 'app-home',
@@ -15,8 +15,11 @@ export interface Coin {
 export class HomeComponent implements OnInit {
   gateKey: string;
   gateSecret: string;
+  coinName: string;
+  address: string;
   displayedColumns: string[] = ['symbol', 'pair', 'rate', 'rate_percent'];
   coins: Array<Coin> = null;
+  balances: any;
   constructor() { }
 
   ngOnInit() {
@@ -46,8 +49,34 @@ export class HomeComponent implements OnInit {
       const res = await axios.request({
         url: `${environment.BaseServerUrl}/get-gate-balances`,
         method: 'post',
+        params: {
+          gateKey: this.gateKey,
+          gateSecret: this.gateSecret
+        }
+      });
+      console.log('res:', res, res.data);
+      this.balances = Object.entries(res.data.available).map((item => {
+        return {coinName: item[0], count: item[1]};
+      }));
+      console.log('balances:', this.balances);
+    } catch (err) {
+      console.log('err:', err);
+    }
+  }
+
+  async getGateCoinAdress() {
+    try {
+      const res = await axios.request({
+        url: `${environment.BaseServerUrl}/get-gate-coinadress`,
+        method: 'post',
+        params: {
+          gateKey: this.gateKey,
+          gateSecret: this.gateSecret,
+          currency: this.coinName
+        }
       });
       console.log('res:', res);
+      this.address = res.data.addr;
     } catch (err) {
       console.log('err:', err);
     }
