92eed9142d4236953f61302334cbe812428269b3
li huan <li.huan@zhtu.net>
2018-08-12 01:03:18 +0800

    添加app home 逻辑

--- project/web/src/app/home/home.service.ts
+++ project/web/src/app/home/home.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor() {
+
+  }
+  
 }
