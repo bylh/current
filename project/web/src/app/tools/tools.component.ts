e4d4eb5c930faea132a2d5a5f4bb96126e05fe3e
li huan <li.huan@zhtu.net>
2018-08-14 21:57:53 +0800

    modify layout

--- project/web/src/app/tools/tools.component.ts
+++ project/web/src/app/tools/tools.component.ts
@@ -1,4 +1,15 @@
+import { ToolsService } from './tools.service';
+import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { AppService } from '../app.service';
+import { MatSnackBar } from '@angular/material';
+export interface Coin {
+  symbol: string; // 代币简称 如ETH
+  pair: string; // 交易对
+  rate: string; // 兑换率
+  rate_percent: string; // 上涨下降比
+}
 
 @Component({
   selector: 'app-tools',
@@ -6,10 +17,92 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tools.component.scss']
 })
 export class ToolsComponent implements OnInit {
+  isLogined: boolean = false;
 
-  constructor() { }
+  gateKey: string;
+  gateSecret: string;
+  coinName: string;
+  address: string;
+  displayedColumns: string[] = ['symbol', 'pair', 'rate', 'rate_percent'];
+  coins: Array<Coin> = null;
+  balances: any;
+  constructor(public appService: AppService, public toolsService: ToolsService, public snackBar: MatSnackBar) {
+    this.isLogined = appService.isLogined();
+    appService.getAuthStateOb().subscribe((user) => {
+      this.isLogined = user != null;
+    })
+  }
 
   ngOnInit() {
+  }
+  async getMarkerList() {
+    try {
+      const res = await axios.request({
+        url: `${environment.BaseServerUrl}/get-gate-marketlist`,
+        method: 'get',
+      });
+      console.log('res:', res);
+      this.coins = res.data.map(coin => {
+        return {
+          symbol: coin.symbol,
+          pair: coin.pair,
+          rate: coin.rate,
+          rate_percent: coin.rate_percent
+        };
+      });
+    } catch (err) {
+      console.log('err:', err);
+    }
+  }
+
+  async getGateBalances() {
+    if (!this.isLogined) {
+      this.snackBar.open('用户未登录');
+      return;
+    }
+    try {
+      const res = await axios.request({
+        url: `${environment.BaseServerUrl}/get-gate-balances`,
+        method: 'post',
+        params: {
+          gateKey: this.gateKey,
+          gateSecret: this.gateSecret
+        }
+      });
+      console.log('res:', res, res.data);
+      this.balances = Object.entries(res.data.available).map((item => {
+        return { coinName: item[0], count: item[1] };
+      }));
+      console.log('balances:', this.balances);
+    } catch (err) {
+      console.log('err:', err);
+    }
+  }
+
+  async getGateCoinAdress() {
+    if (!this.isLogined) {
+      this.snackBar.open('用户未登录');
+      return;
+    }
+    if (this.coinName == null) {
+      this.snackBar.open('请输入代币名称');
+      return;
+    }
+    try {
+      const res = await axios.request({
+        url: `${environment.BaseServerUrl}/get-gate-coinadress`,
+        method: 'post',
+        params: {
+          gateKey: this.gateKey,
+          gateSecret: this.gateSecret,
+          currency: this.coinName
+        }
+      });
+      console.log('res:', res);
+      this.address = res.data.addr;
+    } catch (err) {
+      console.log('err:', err);
+    }
   }
 
 }
--- project/web/src/app/tools/tools.module.ts
+++ project/web/src/app/tools/tools.module.ts
@@ -1,13 +1,18 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ToolsRoutingModule } from './tools-routing.module';
 import { ToolsComponent } from './tools.component';
+import { AppMaterialModule } from '../app-material.module';
+import { FormsModule } from '../../../node_modules/@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    ToolsRoutingModule
+    ToolsRoutingModule,
+    AppMaterialModule,
+    FormsModule
   ],
   declarations: [ToolsComponent]
 })
