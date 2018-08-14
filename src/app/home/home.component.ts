3210e183542c3d3c0442fba698977bfa12bc62a1
li huan <li.huan@zhtu.net>
2018-08-14 21:57:53 +0800

    modify layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,12 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import { AppService } from '../app.service';
 import { MatSnackBar } from '@angular/material';
-export interface Coin {
-  symbol: string; // 代币简称 如ETH
-  pair: string; // 交易对
-  rate: string; // 兑换率
-  rate_percent: string; // 上涨下降比
-}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,13 +13,6 @@ export interface Coin {
 export class HomeComponent implements OnInit {
   isLogined: boolean = false;
 
-  gateKey: string;
-  gateSecret: string;
-  coinName: string;
-  address: string;
-  displayedColumns: string[] = ['symbol', 'pair', 'rate', 'rate_percent'];
-  coins: Array<Coin> = null;
-  balances: any;
   constructor(public appService: AppService, public homeService: HomeService, public snackBar: MatSnackBar) {
     this.isLogined = appService.isLogined();
     appService.getAuthStateOb().subscribe((user) => {
@@ -34,74 +22,4 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
-  async getMarkerList() {
-    try {
-      const res = await axios.request({
-        url: `${environment.BaseServerUrl}/get-gate-marketlist`,
-        method: 'get',
-      });
-      console.log('res:', res);
-      this.coins = res.data.map(coin => {
-        return {
-          symbol: coin.symbol,
-          pair: coin.pair,
-          rate: coin.rate,
-          rate_percent: coin.rate_percent
-        };
-      });
-    } catch (err) {
-      console.log('err:', err);
-    }
-  }
-
-  async getGateBalances() {
-    if (!this.isLogined) {
-      this.snackBar.open('用户未登录');
-      return;
-    }
-    try {
-      const res = await axios.request({
-        url: `${environment.BaseServerUrl}/get-gate-balances`,
-        method: 'post',
-        params: {
-          gateKey: this.gateKey,
-          gateSecret: this.gateSecret
-        }
-      });
-      console.log('res:', res, res.data);
-      this.balances = Object.entries(res.data.available).map((item => {
-        return { coinName: item[0], count: item[1] };
-      }));
-      console.log('balances:', this.balances);
-    } catch (err) {
-      console.log('err:', err);
-    }
-  }
-
-  async getGateCoinAdress() {
-    if (!this.isLogined) {
-      this.snackBar.open('用户未登录');
-      return;
-    }
-    if (this.coinName == null) {
-      this.snackBar.open('请输入代币名称');
-      return;
-    }
-    try {
-      const res = await axios.request({
-        url: `${environment.BaseServerUrl}/get-gate-coinadress`,
-        method: 'post',
-        params: {
-          gateKey: this.gateKey,
-          gateSecret: this.gateSecret,
-          currency: this.coinName
-        }
-      });
-      console.log('res:', res);
-      this.address = res.data.addr;
-    } catch (err) {
-      console.log('err:', err);
-    }
-  }
-
 }
