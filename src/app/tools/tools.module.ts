e09b9825151933f09e146698ac186caadcbd3ae9
li huan <li.huan@zhtu.net>
2018-08-25 13:26:35 +0800

    临时提交

--- src/app/tools/tools.module.ts
+++ src/app/tools/tools.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 
 import { ToolsRoutingModule } from './tools-routing.module';
 import { ToolsComponent } from './tools.component';
-import { AppMaterialModule } from '../app-shared.module';
+import { AppSharedModule } from '../app-shared.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     ToolsRoutingModule,
-    AppMaterialModule,
+    AppSharedModule,
     FormsModule
   ],
   declarations: [ToolsComponent]
