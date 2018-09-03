df6b634c16249b5f286d86d214e9276ea41ff3d1
lihuan <li.huan@zhtu.net>
2018-09-03 18:41:55 +0800

    interview

---
+++ code/interview/js-html-css/test.js
@@ -0,0 +1,19 @@
+function fn1() {
+    for (var i = 0; i < 4; i++) {
+        var tc = setTimeout(function (i) {
+            console.log(i);
+            clearTimeout(tc)
+        }, 10, i);
+    }
+}
+
+function fn2() {
+    for (var i = 0; i < 4; i++) {
+        var tc = setInterval(function (i, tc) {
+            console.log(i);
+            clearInterval(tc)
+        }, 10, i, tc);
+    }
+}
+fn1();
+// fn2();
