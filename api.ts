c88e9b4673676cd407a60ce16c8e02df1a3fa059
li huan <li.huan@zhtu.net>
2018-09-08 13:51:08 +0800

    测试多图上传demo

--- api.ts
+++ api.ts
@@ -272,7 +272,12 @@ export async function startGateAutoTrade(req: express.Request, res: express.Resp
 export async function uploadImg(req: express.Request, res: express.Response) {
     try {
         console.log('开始上传', req.body, req.file);
-        
+        let file = req.file;
+
+        console.log('文件类型：%s', file.mimetype);
+        console.log('原始文件名：%s', file.originalname);
+        console.log('文件大小：%s', file.size);
+        console.log('文件保存路径：%s', file.path);
         console.log('上传成功');
         res.sendStatus(200);
     } catch (err) {
