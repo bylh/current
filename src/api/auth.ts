fa924e16cef1274eabbefccff49849dae95981ad
li huan <li.huan@zhtu.net>
2018-09-08 18:20:35 +0800

    服务器代码结构调整

---
+++ src/api/auth.ts
@@ -0,0 +1,176 @@
+
+const webpush = require('web-push');
+import express from 'express';
+import { Defer } from '../common/common';
+import DBHelper, { WebPushInfo, userModel } from '../common/db-helper';
+import Config from '../config';
+import { Payload, PayloadTest } from '../define/webpush';
+
+export async function checkSession(req: express.Request, res: express.Response, next: express.NextFunction) {
+    if (req.session.userId == null) {
+        res.sendStatus(401);
+        return;
+    }
+    next(); // 权限验证成功
+}
+export async function signUp(req: express.Request, res: express.Response) {
+    try {
+        console.log('开始注册', req.body);
+        let result = await DBHelper.getOne('user', { userId: req.body.userId });
+        if (result != null) {
+            console.log('用户已经存在');
+            res.sendStatus(409);
+            return;
+        }
+        await DBHelper.update(req.body, 'user')
+        console.log('注册成功');
+        res.sendStatus(200);
+    } catch (err) {
+        console.log('注册失败');
+        res.sendStatus(500);
+    }
+}
+
+export async function login(req: express.Request, res: express.Response) {
+    try {
+        console.log('开始登录', req.body);
+        let result = await DBHelper.getOne('user', { userId: req.body.userId, pwd: req.body.pwd });
+        if (result != null) {
+            console.log('登录成功前', req.session, req.sessionID);
+            req.session.userId = req.body.userId;  //设置session
+            console.log('登录成功后', req.session, req.sessionID);
+            res.sendStatus(200);
+            return;
+        } else {
+            console.log('找不到用户');
+            res.sendStatus(404);
+            return;
+        }
+
+    } catch (err) {
+        console.log('登录失败');
+        res.sendStatus(500);
+    }
+}
+
+export async function resetPwd(req: express.Request, res: express.Response) {
+    try {
+        console.log('开始重置密码', req.body);
+        if (req.session.userId == null) {
+            res.sendStatus(401);
+            console.log('未登录不能重置密码');
+            return;
+        }
+        let defer = new Defer<boolean>();
+        userModel.findOne({ userId: req.session.userId }, (err, res) => {
+            console.log('getOne(): res:', res, 'err', err, (res as any).pwd);
+            defer.resolve((res as any).pwd === req.body.orgPwd);
+        });
+        let match = await defer.promise;
+        if (!match) {
+            console.log('原密码输入错误');
+            res.sendStatus(402);
+            return;
+        }
+        await userModel.update({ userId: req.session.userId }, { pwd: req.body.newPwd });
+        console.log('重置成功');
+        res.sendStatus(200);
+        return;
+    } catch (err) {
+        console.log('重置失败');
+        res.sendStatus(500);
+    }
+}
+
+export async function uploadImg(req: express.Request, res: express.Response) {
+    try {
+        console.log('开始上传', req.body, req.file);
+        let file = req.file;
+
+        console.log('文件类型：%s', file.mimetype);
+        console.log('原始文件名：%s', file.originalname);
+        console.log('文件大小：%s', file.size);
+        console.log('文件保存路径：%s', file.path);
+        console.log('上传成功');
+        res.sendStatus(200);
+    } catch (err) {
+        console.log('失败');
+        res.sendStatus(500);
+    }
+}
+
+export async function getProfile(req: express.Request, res: express.Response) {
+    try {
+        console.log('获取个人资料开始', req.body);
+        let profile = await DBHelper.getOne('user', { userId: req.body.userId });
+        if (profile) {
+            console.log('获取个人资料成功');
+            res.sendStatus(200);
+            return;
+        } else {
+            console.log('找不到用户');
+            res.sendStatus(404);
+            return;
+        }
+
+    } catch (err) {
+        console.log('获取个人资料失败');
+        res.sendStatus(500);
+    }
+}
+
+export async function updateProfile(req: express.Request, res: express.Response) {
+    try {
+        console.log('更新个人资料开始', req.body);
+        let profile = await DBHelper.update({ info: req.body.info }, 'profile', { userId: req.body.userId });
+        if (profile) {
+            console.log('更新个人资料成功', profile);
+            res.sendStatus(200);
+            return;
+        } else {
+            console.log('找不到用户');
+            res.sendStatus(404);
+            return;
+        }
+
+    } catch (err) {
+        console.log('更新个人资料失败');
+        res.sendStatus(500);
+    }
+}
+
+
+
+// 订阅相关
+export async function subscribe(req: express.Request, res: express.Response) {
+    let query = req.query;
+    console.log('收到', query.pushSubscription, query.userId);
+    let document = await DBHelper.getOne('webpush', { pushSubscription: query.pushSubscription });
+    if (document != null) {
+        console.log('用户已经订阅过了');
+        res.status(200).json(req.query);
+        return;
+    }
+    // 说明此设备有新的订阅需求，或者第一次订阅
+    await DBHelper.set('webpush', {
+        userId: query.userId,
+        pushSubscription: query.pushSubscription
+    } as WebPushInfo);
+    sendNotification(query.pushSubscription, Payload)
+    res.status(200).json(req.query);
+}
+
+export function sendNotification(pushSubscription: string, payload: any) {
+    webpush.setGCMAPIKey(Config.Push.GcmApiKey);
+    webpush.setVapidDetails(Config.Push.Subject, Config.Push.PublicKey, Config.Push.PrivateKey);
+    webpush.sendNotification(JSON.parse(pushSubscription), JSON.stringify(payload)).then((suc: any) => console.log('成功', suc)).catch((err: any) => console.log('失败', err));
+}
+
+export async function sendNotificationToUsers(req: express.Request, res: express.Response) {
+    let subs = await DBHelper.getAll('webpush', { userId: req.session.userId });
+    console.log('所有订阅', subs);
+    for (let sub of subs) {
+        sendNotification((sub as any).pushSubscription, PayloadTest)
+    }
+    res.status(200).json(PayloadTest);
+}
--- src/common/db-helper.ts
+++ src/common/db-helper.ts
@@ -1,36 +1,50 @@
 import mongoose, { Mongoose, Document } from 'mongoose';
 import { Defer } from './common';
-import Config from './config';
-export interface WebPushInfo {
+import Config from '../config';
+export interface WebPushInfo { // 推送信息
     userId: string,
     pushSubscription: string
 }
-// ${Config.Server.IP}
+
+export type ModelType = 'user' | 'profile' | 'webpush'; // 数据类型
+
+// 数据库URL
 export const CollectUri = `mongodb://${Config.Server.DB.User}:${Config.Server.DB.Pwd}@127.0.0.1:${Config.Server.DB.Port}/web`;
+
 const Schema = mongoose.Schema;
+
+// 推送
 const webPushSchema = new Schema({
     userId: String,
     pushSubscription: String
 });
+
 const userSchema = new Schema({
     userId: String,
-    pwd: String, // 可能用不到
-    isAdmin: {
+    pwd: String,
+    isAdmin: {  // 是否为管理员
         type: Boolean,
         default: false
     },
     info: {
-        avatarUrl: String,
-        bgUrl: String,
-        description: String,
-        age: String,
+        avatarUrl: String, // 头像url
+        bgUrl: String,  // 背景url
+        description: String, // 描述
+        age: String, // 年龄
+        phone: String, // 手机号
+        email: String, // 邮箱
+        wechat: String, // 微信
     }
 
-})
+});
+
 const webPushModel = mongoose.model('webpushes', webPushSchema);
+
 export const userModel = mongoose.model('users', userSchema);
+
 class DBHelper {
     protected db: Mongoose = null;
+
     // 初始化连接数据库
     public async init() {
         try {
@@ -39,37 +53,45 @@ class DBHelper {
             throw err;
         }
     }
-    public async findOne(type: string, conditions: any): Promise<boolean>{
-        let defer = new Defer<boolean>();
+
+    public getModel(type: ModelType): mongoose.Model<mongoose.Document> {
         let model: mongoose.Model<mongoose.Document> = null;
-        if(type === 'user') {
-            model = userModel;
-        } else if(type === 'webpush') {
-            model = webPushModel;
+        switch (type) {
+            case 'user':
+                model = userModel;
+                break;
+            case 'profile':
+                model = userModel;
+                break;
+            case 'webpush':
+                model = webPushModel;
+                break;
         }
-        model.findOne(conditions, (err, res) => {
-            console.log('getOne(): res:', res, 'err', err);
-            defer.resolve(res != null);
-        });
-        return await defer.promise;;
+        return model;
     }
-    public async getOne(conditions: any) {
+
+    public async getOne(type: ModelType, conditions: any): Promise<mongoose.Document> {
+        let model = this.getModel(type);
         let defer = new Defer<any>();
-        webPushModel.findOne(conditions, (err, res) => {
+        model.findOne(conditions, (err, res) => {
             console.log('getOne(): res:', res, 'err', err);
             defer.resolve(res);
         });
         return await defer.promise;;
     }
-    public async getAll(conditions?: any) {
+
+    public async getAll(type: ModelType, conditions?: any): Promise<Array<mongoose.Document>> {
         let defer = new Defer<any>();
-        webPushModel.find(conditions, (err, res) => {
+        let model = this.getModel(type);
+
+        model.find(conditions, (err, res) => {
             console.log('getAll(): res:', res, 'err', err);
             defer.resolve(res);
         });
-        return await defer.promise;;
+        return await defer.promise;
     }
-    public async set(info: any, type: 'user' | 'webpush' = 'webpush'): Promise<any> {
+
+    public async set(type: ModelType, info: any): Promise<any> { // 区别于update，直接保存
         let data;
         if (type === 'user') {
             data = new userModel(info);
@@ -84,29 +106,24 @@ class DBHelper {
         console.log('保存数据成功', info);
         return info;
     }
+
     public async update(info: any, type: 'user' | 'profile' | 'webpush' = 'webpush', conditions: any = null) {
-        let data;
         if (type === 'user') {
-            data = new userModel(info);
             try {
                 await userModel.update({
                     userId: info.userId,
                     pwd: info.pwd
-                }, info, {upsert: true},(err, raw) => console.log(err, raw));
+                }, info, { upsert: true }, (err, raw) => console.log(err, raw));
             } catch (err) {
                 throw err;
             }
-        } else if(type === 'profile') {
+        } else if (type === 'profile') {
             try {
-                console.log('profile数据库更新', conditions, {info: info});
-                await userModel.update(conditions, {info: info}, {upsert: true},(err, raw) => console.log(err, raw));
-                // await data.update(info);
+                console.log('profile数据库更新', conditions, { info: info });
+                await userModel.update(conditions, info, { upsert: true }, (err, raw) => console.log(err, raw));
             } catch (err) {
                 throw err;
             }
-
-        } else if (type === 'webpush') {
-            data = new webPushModel(info);
         }
         console.log('保存数据成功', info);
         return info;
