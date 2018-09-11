dfc678591dedfdd9c7bc2bc41a5744499be403ef
li huan <li.huan@zhtu.net>
2018-09-11 22:55:15 +0800

    添加markdown编辑器

--- src/app/discovery/discovery-detail/discovery-detail.component.ts
+++ src/app/discovery/discovery-detail/discovery-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
+
+import Editor from 'tui-editor';
 
 @Component({
   selector: 'app-discovery-detail',
@@ -8,9 +10,11 @@ import { Location } from '@angular/common';
   styleUrls: ['./discovery-detail.component.scss']
 })
 export class DiscoveryDetailComponent implements OnInit {
+  @ViewChild('editSection') editElementRef: ElementRef;
 
   public id: number;
 
+  public editor: Editor;
   constructor(private route: ActivatedRoute, private router: Router, private location: Location) {
   }
 
@@ -18,6 +22,13 @@ export class DiscoveryDetailComponent implements OnInit {
     console.log('ngOnInit(): discovery detail init');
     this.route.paramMap.subscribe((params => this.id = +params.get('id')));
     // this.id = +this.route.snapshot.paramMap.get('id'); // + 将string转化为number
+
+    this.editor = new Editor({
+      el: this.editElementRef.nativeElement,
+      initialEditType: 'markdown',
+      previewStyle: 'vertical',
+      height: '300px'
+  });
   }
   back() {
     console.log('backId:', this.id);
