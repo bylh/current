92deed70896166e60750f5bc8d415fa45193d324
li huan <li.huan@zhtu.net>
2018-09-30 01:01:47 +0800

    爬虫测试

--- src/utiles/temp.ts
+++
@@ -1,4 +0,0 @@
-var GateIo = require('gate.io');
-var gateIoClient = new GateIo('3E32736D-29B0-49F6-8954-F91752C659FB', 'a15a4a100a510542c82e3d4ef48ceddbcdd33319d0a2db47ed967d84973ccc99');
-
-gateIoClient.getBalances(res => console.log(res));
