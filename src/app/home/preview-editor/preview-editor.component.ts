55f431bb4caf80f271e8e6b3da2546b20e838c7a
lihuan <li.huan@zhtu.net>
2018-10-03 00:59:05 +0800

    add fontawesome && tui-editor extensions

--- src/app/home/preview-editor/preview-editor.component.ts
+++ src/app/home/preview-editor/preview-editor.component.ts
@@ -5,6 +5,12 @@ import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { AuthService } from '../../auth.service';
 import Editor from 'tui-editor';
+import 'tui-editor/dist/tui-editor-extScrollSync'; // extensions 同步滚动
+import 'tui-editor/dist/tui-editor-extChart'; // extensions 图表支持
+import 'tui-editor/dist/tui-editor-extUML'; // extensions 流程图支持
+import 'plantuml-encoder/lib/plantuml-encoder' // 流程图支持必须有这个库
+import 'tui-editor/dist/tui-editor-extColorSyntax'; // extensions 着色器支持
+
 @Component({
   selector: 'preview-editor',
   templateUrl: './preview-editor.component.html',
@@ -32,6 +38,7 @@ export class PreviewEditorComponent implements OnInit {
         previewStyle: 'vertical', // tab  vertical
         initialEditType: 'wysiwyg', // markdown, wysiwyg
         // initialValue: '# hello', // 这个初始值为markdown
+        exts: ['scrollSync', 'colorSyntax', 'uml'], // 同步滚动
         language: 'zh',
         height: '100%',
         width: '100%'
