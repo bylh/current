5073ab5b50523272bd1c81f2bf6af4bf6057c14f
lihuan <li.huan@zhtu.net>
2018-10-05 00:45:48 +0800

    test

--- sub/bit-server/src/utils/test.ts
+++ sub/bit-server/src/utils/test.ts
@@ -8,21 +8,11 @@ import cors from 'cors';
     app.use(cors()); // 解决跨域访问的问题
     app.use('/data', getData);
     // 启动监听
-    app.listen(4000);
+    app.listen(5000);
     if (process.send != null) process.send('ready');
 
-    console.log('监听4000端口');
+    console.log('监听5000端口');
 
-    let res = await axios.request({
-        // http://www.bylh.top:4000/data
-        url: `http://www.bylh.top:4000/data`,
-        method: 'get',
-
-        // params: {
-        //   market_code: 'ocxeth'
-        // },
-    });
-    console.log(res.data);
 
     process.on('SIGINT', async () => {  // 保存log后退出
         process.exit(); // 程序结束
@@ -35,8 +25,4 @@ async function getData(req: express.Request, res: express.Response) {
         method: 'get',
     });
     res.status(200).json(response.data);
-    // res.sendStatus(200);
-    // CircularJSON.stringify(response)
-    // res.status(200).send(JSON.parse(CircularJSON.stringify(response)));
-    // res.status(200).send(eval(CircularJSON.stringify(response)))
 }
