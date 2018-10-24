dc86672fbdebbe6086dd7a4074ccdaecb05c8ba2
li huan <li.huan@zhtu.net>
2018-10-25 02:24:59 +0800

    修改更新文章逻辑

--- sub/bit-client/src/app/tabs/tabs.component.ts
+++ sub/bit-client/src/app/tabs/tabs.component.ts
@@ -1,3 +1,4 @@
+import { HomeService } from './../home/home.service';
 import { async } from '@angular/core/testing';
 
 import { Router } from '@angular/router';
@@ -22,15 +23,16 @@ export class TabsComponent implements AfterViewInit, OnInit {
   avatarUrl: string;
   constructor(
     protected appService: AppService,
+    protected homeService: HomeService,
     public auth: AuthService,
     protected router: Router,
     protected location: Location,
     public snackBar: MatSnackBar
   ) {
-    this.auth.getAuthSubject().subscribe( async(userId) => {
+    this.auth.getAuthSubject().subscribe(async (userId) => {
       this.userId = userId;
       this.avatarUrl = `${environment.BaseServerUrl}/avatars/${userId}-avatar.jpg`;
-      if(userId != null) {
+      if (userId != null) {
         try {
           const pushSubscription = await this.appService.subscribeUser();
           console.log('订阅信息并保存到服务器（成功状态）：', JSON.stringify(pushSubscription));
@@ -49,7 +51,7 @@ export class TabsComponent implements AfterViewInit, OnInit {
     try {
       let logined = await this.auth.isLogined();
       console.log('是否登录', logined);
-    } catch(err) {
+    } catch (err) {
       console.log('检查是否登录出错', err);
     }
     console.log('ngOninit(): 获取订阅，可能之前订阅，也有可能是新的订阅，总之都返回订阅信息');
@@ -85,12 +87,20 @@ export class TabsComponent implements AfterViewInit, OnInit {
       console.log('账户登出出错', err);
     }
   }
-  
+
   public back() {
     this.location.back();
 
   }
-  public reload() {
+  public async reload() {
+    // TODO 临时解决方案，待完善
+    try {
+      await this.homeService.getArticles(true);
+      console.log('拉取文章成功');
+    } catch(err) {
+      console.log('拉取文章失败', err);
+    }
+   
     window.location.reload();
   }
 }
