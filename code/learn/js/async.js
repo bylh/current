b0ea80e4891a3e8d5c3df138d08e78f746b09746
lihuan <li.huan@zhtu.net>
2018-10-24 12:15:39 +0800

    临时提交

---
+++ code/learn/js/async.js
@@ -0,0 +1,18 @@
+
+  (async function main(params) {
+    const f = () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(123);
+          }, 2000);
+        });
+      };
+      
+      const testAsync = async () => {
+        const t = await f();
+        console.log(t);
+      };
+      
+      testAsync();
+      console.log('hello');
+  })();
