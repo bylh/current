e2e12c4fb65a3636f66fb38158f3c10cdf4d9238
lihuan <li.huan@zhtu.net>
2018-11-01 18:23:15 +0800

    add progressbar

--- src/app/discovery/discovery.component.ts
+++ src/app/discovery/discovery.component.ts
@@ -1,3 +1,4 @@
+import { MatSnackBar } from '@angular/material';
 import { DiscoveryService } from './discovery.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router} from '@angular/router';
@@ -28,7 +29,13 @@ export class DiscoveryComponent implements OnInit {
 
   public segs;
 
-  constructor(private route: ActivatedRoute, private router: Router, private discoveryService: DiscoveryService) {
+  isLoaded: boolean = false;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
+    private router: Router, 
+    private discoveryService: DiscoveryService) {
     this.route.paramMap.subscribe(params => console.log('discovery: backId', params.get('backId')))
    }
 
@@ -44,7 +51,10 @@ export class DiscoveryComponent implements OnInit {
       this.segs = await this.discoveryService.getSeg();
       console.log(this.segs);
     }catch(err) {
-      console.log('getSeg(): error', err)
+      console.log('getSeg(): error', err);
+      this.snackBar.open('获取数据失败');
+    } finally {
+      this.isLoaded = true;
     }
   }
 
